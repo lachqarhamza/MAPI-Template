20b2060f329c47555b4c6d66ae5109feb131a947
LACHQAR HAMZA <lachquarhamza@gmail.com>
2023-09-11 14:28:39 +0100

    fix the index.js

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-import express from "express";
-import dotenv from "dotenv";
-import logger from "morgan";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import dotenv from 'dotenv'
+import logger from 'morgan'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import features from './features'
+import middlewares from './middlewares'
 
-import authRouter from "./features/auth/router";
-//import merchantsController from './features/merchants/router'
-import ratingsRouter from "./features/ratings/router";
+dotenv.config()
 
-import middlewares from "./middlewares";
-
-dotenv.config();
-
-if (process.env.NODE_ENV !== "test") {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
-    .then(() => console.log("MongoDB connected..."))
-    .catch((err) => console.log("MongoDb Error: ", err.message));
+if (process.env.NODE_ENV !== 'test') {
+	mongoose
+		.connect(process.env.DB_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		})
+		.then(() => console.log('MongoDB connected...'))
+		.catch(err => console.log('MongoDb Error: ', err.message))
 }
 
-let app = express();
-app.disable("etag");
-app.enable("trust proxy");
-app.use(logger("dev"));
-app.use(express.static("public", { etag: false }));
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+let app = express()
+app.disable('etag')
+app.enable('trust proxy')
+app.use(logger('dev'))
+app.use(express.static('public', { etag: false }))
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
-app.use("/admin/auth", middlewares.adminPreAuth, authRouter);
-app.use("/auth", middlewares.preAuth, authRouter);
-// app.use('/merchants', middlewares.isAuth, merchantsController)
-app.use("/ratings", middlewares.isAuth, ratingsRouter);
+app.use('/exampleRoute', middlewares.isAuth, features.exampleFeature.router)
 
-const PORT = process.env.serverPort;
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
+const PORT = process.env.SERVER_PORT
+if (process.env.NODE_ENV !== 'test') {
+	app.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}`)
+	})
 }
 
-export default app;
+export default app
